5e637767ca081cb5284e1fecba5ec36e81089aa1
Zhandroid <mathew.collins@gmail.com>
2024-12-14 16:41:49 -0500

    Update instructions

--- template/light/tpl/howtoconnect.php
+++ template/light/tpl/howtoconnect.php
@@ -8,7 +8,7 @@
 ?>
 <div class="content_box1" style="line-height: 1.5;">
     <p>1. Create an account by selecting Register above.</p>
-    <p>2. You can download the client via a publicly distributed torrent magnet (<a href="magnet:?xt=urn:btih:lns5deukgas2qifulzw3eri2vkv4knd4&dn=World%20of%20Warcraft%203.3.5a&xl=17855168814&fc=165">Torrent magent for 3.3.5a</a>) or use our private tracker (<a href="World.of.Warcraft.3.3.5a.torrent">Private Torrent</a>)</p>
+    <p>2. You can download the client via a publicly distributed torrent magnet (<a href="magnet:?xt=urn:btih:lns5deukgas2qifulzw3eri2vkv4knd4&dn=World%20of%20Warcraft%203.3.5a&xl=17855168814&fc=165" rel="nofollow">Torrent magent for 3.3.5a</a>) or use our private tracker (<a href="World.of.Warcraft.3.3.5a.torrent" rel="nofollow">Private Torrent</a>) If you need a torrent client, consider using simple and lightweight <a href="https://www.qbittorrent.org/download" rel="nofollow">qBittorrent</a></p>
     <p>3. <?php elang('create_account_tip3'); ?></p>
     <p>4. Open the realmlist.wtf file with notepad and set the following: </p>
     <p style="text-align: center;font-weight: bold;color:darkred">set realmlist <?php echo get_config('realmlist'); ?></p>
