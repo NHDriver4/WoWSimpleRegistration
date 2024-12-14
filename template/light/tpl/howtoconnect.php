c9f4841bda258f122498be3ea446234c2dbe40d2
Zhandroid <mathew.collins@gmail.com>
2024-12-14 16:23:41 -0500

    Adding magnet for download

--- template/light/tpl/howtoconnect.php
+++ template/light/tpl/howtoconnect.php
@@ -7,8 +7,8 @@
  */
 ?>
 <div class="content_box1" style="line-height: 1.5;">
-    <p>1. <?php elang('create_account_tip1'); ?></p>
-    <p>2. <?php elang('create_account_tip2'); ?></p>
+    <p>1. <?php elang('create_account_tip1'); ?>     </p>
+    <p>2. <?php elang('create_account_tip2'); ?> <a href="magnet:?xt=urn:btih:lns5deukgas2qifulzw3eri2vkv4knd4&dn=World%20of%20Warcraft%203.3.5a&xl=17855168814&fc=165">Torrent magent for 3.3.5a</ad></p>
     <p>3. <?php elang('create_account_tip3'); ?></p>
     <p>4. <?php elang('create_account_tip4'); ?></p>
     <p style="text-align: center;font-weight: bold;color:darkred">set
